a92187283c8b36547c63c31522e4e9ec8b7c1e5f
Hamid Ali Khan <hak.hamidali14@gmail.com>
2026-03-19 14:01:24 +0500

    Added failing test for countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -34,4 +34,16 @@ public class CustomListTest {
             list.deleteCity(calgary);
         });
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        assertEquals(2, list.countCities());
+    }
 }
